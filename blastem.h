706724564ad0d0c356d6cce7d610782a7f28d749
Mike Pavone <pavone@retrodev.com>
2013-07-09 20:51:42 -0700

    Added ternary tree implementation and a simple test program for it

---
+++ blastem.h
@@ -0,0 +1,38 @@
+#ifndef BLASTEM_H_
+#define BLASTEM_H_
+
+#include <stdint.h>
+#include "m68k_to_x86.h"
+#include "z80_to_x86.h"
+#include "ym2612.h"
+#include "vdp.h"
+#include "psg.h"
+#include "io.h"
+
+#define RAM_FLAG_ODD  0x1800
+#define RAM_FLAG_EVEN 0x1000
+#define RAM_FLAG_BOTH 0x0000
+
+#define CYCLE_NEVER 0xFFFFFFFF
+
+typedef struct {
+	m68k_context   *m68k;
+	z80_context    *z80;
+	vdp_context    *vdp;
+	ym2612_context *ym;
+	psg_context    *psg;
+	uint8_t        *save_ram;
+	uint32_t       save_ram_mask;
+	uint32_t       save_flags;
+	uint8_t        bank_regs[8];
+	io_port        ports[3];
+} genesis_context;
+
+extern genesis_context * genesis;
+extern int break_on_sync;
+
+uint16_t read_dma_value(uint32_t address);
+m68k_context * debugger(m68k_context * context, uint32_t address);
+
+#endif //BLASTEM_H_
+
---
+++ x86_backend.h
@@ -0,0 +1,62 @@
+#ifndef X86_BACKEND_H_
+#define X86_BACKEND_H_
+
+#include <stdint.h>
+
+#define INVALID_OFFSET 0xFFFFFFFF
+#define EXTENSION_WORD 0xFFFFFFFE
+
+typedef struct {
+	int32_t disp;
+	uint8_t mode;
+	uint8_t base;
+	uint8_t index;
+	uint8_t cycles;
+} x86_ea;
+
+typedef struct {
+	uint8_t  *base;
+	int32_t  *offsets;
+} native_map_slot;
+
+typedef struct deferred_addr {
+	struct deferred_addr *next;
+	uint8_t              *dest;
+	uint32_t             address;
+} deferred_addr;
+
+
+#define MMAP_READ      0x01
+#define MMAP_WRITE     0x02
+#define MMAP_CODE      0x04
+#define MMAP_PTR_IDX   0x08
+#define MMAP_ONLY_ODD  0x10
+#define MMAP_ONLY_EVEN 0x20
+#define MMAP_FUNC_NULL 0x40
+
+typedef uint16_t (*read_16_fun)(uint32_t address, void * context);
+typedef uint8_t (*read_8_fun)(uint32_t address, void * context);
+typedef void * (*write_16_fun)(uint32_t address, void * context, uint16_t value);
+typedef void * (*write_8_fun)(uint32_t address, void * context, uint8_t value);
+
+typedef struct {
+	uint32_t     start;
+	uint32_t     end;
+	uint32_t     mask;
+	uint16_t     ptr_index;
+	uint16_t     flags;
+	void *       buffer;
+	read_16_fun  read_16;
+	write_16_fun write_16;
+	read_8_fun   read_8;
+	write_8_fun  write_8;
+} memmap_chunk;
+
+typedef uint8_t * (*native_addr_func)(void * context, uint32_t address);
+
+deferred_addr * defer_address(deferred_addr * old_head, uint32_t address, uint8_t *dest);
+void remove_deferred_until(deferred_addr **head_ptr, deferred_addr * remove_to);
+void process_deferred(deferred_addr ** head_ptr, void * context, native_addr_func get_native);
+
+#endif //X86_BACKEND_H_
+
