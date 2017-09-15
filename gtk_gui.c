5345c566e00e8ecf55c7dc4a375af53a67b8a686
AmatCoder <amatcoder@gmail.com>
2017-09-15 23:13:26 +0200

    Redraw socket

--- gtk_gui.c
+++ gtk_gui.c
@@ -24,7 +24,7 @@ void show_chooser(GtkMenuItem *menuitem, gpointer data)
   char* rom = NULL;
 
   dialog = gtk_file_chooser_dialog_new(
-    "Choose a rom...", GTK_WINDOW(data),
+    "Choose a ROM...", GTK_WINDOW(gtk_widget_get_toplevel(data)),
     GTK_FILE_CHOOSER_ACTION_OPEN, ("_Cancel"),
     GTK_RESPONSE_CANCEL, ("_Open"), GTK_RESPONSE_ACCEPT,
     NULL);
@@ -33,12 +33,20 @@ void show_chooser(GtkMenuItem *menuitem, gpointer data)
   //gtk_file_filter_add_pattern(filter, "*.*");
   //gtk_file_chooser_set_filter(GTK_FILE_CHOOSER(exe), filter);
 
+  gtk_widget_hide(data);
+
   if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT)
   {
     rom = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER (dialog));
   }
 
   gtk_widget_destroy (dialog);
+
+  gtk_widget_show(data);
+
+  gtk_widget_set_can_focus (data, TRUE);
+  gtk_widget_grab_focus (data);
+  gtk_widget_set_can_focus (data, FALSE);
 
   if (rom)
     load(rom);
@@ -47,6 +55,7 @@ void show_chooser(GtkMenuItem *menuitem, gpointer data)
 void create_gui(unsigned long XID, int width, int height)
 {
   GtkWidget *window;
+  GtkWidget *frame;
   GtkWidget *socket;
 
   GtkWidget *vbox;
@@ -59,6 +68,7 @@ void create_gui(unsigned long XID, int width, int height)
   gtk_init(NULL, NULL);
 
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  frame = gtk_frame_new (NULL);
   socket = gtk_socket_new();
   vbox = gtk_vbox_new(FALSE, 0);
 
@@ -76,20 +86,22 @@ void create_gui(unsigned long XID, int width, int height)
 
   gtk_box_pack_start(GTK_BOX(vbox), menubar, FALSE, FALSE, 0);
   gtk_container_add(GTK_CONTAINER(window), vbox);
-  gtk_container_add (GTK_CONTAINER (vbox), socket);
+  gtk_container_add(GTK_CONTAINER(vbox), frame);
+  gtk_container_add (GTK_CONTAINER (frame), socket);
 
   g_signal_connect(window, "delete-event", G_CALLBACK(delete_event), NULL);
   g_signal_connect(quit, "activate", G_CALLBACK(quit_gui), window);
-  g_signal_connect(open, "activate", G_CALLBACK(show_chooser), window);
+  g_signal_connect(open, "activate", G_CALLBACK(show_chooser), socket);
 
   gtk_widget_set_size_request(socket, width, height);
 
   GdkRGBA bg = { 0, 0, 0, 1 };
-  gtk_widget_override_background_color(socket, GTK_STATE_FLAG_NORMAL, &bg);
+  gtk_widget_override_background_color(frame, GTK_STATE_FLAG_NORMAL, &bg);
 
   gtk_socket_add_id(GTK_SOCKET(socket), XID);
 
   gtk_widget_show_all(window);
+  gtk_widget_hide(socket);
 
   gtk_main();
 }
